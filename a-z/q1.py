261cdb928056e86880aae7a988996d4bc6d2e2b2
ldynia <ludd@cbs.dtu.dk>
2020-07-14 14:20:42 +0200

    adding az test

---
+++ a-z/q1.py
@@ -0,0 +1,37 @@
+#!/#!/usr/bin/env python3
+
+"""
+amazon test id 23280666807354
+https://www.youtube.com/watch?v=ED4ateJu86I
+https://leetcode.com/problems/partition-labels/
+"""
+
+import math
+from collections import OrderedDict
+
+
+def closestLocations(totalCrates, allLocations, truckCapacity):
+    origin = [0, 0]
+    distances = {}
+    # O(n)
+    for location in allLocations:
+        x = location[0] - origin[0]
+        y = location[1] - origin[1]
+        distance = math.sqrt(x**2 + y**2)
+        distances[distance] = location
+
+    # O(log n) or # O(nlog n)
+    sorted_distance = sorted(distances.items())
+
+    # O(n)
+    results = []
+    for d in sorted_distance:
+        if len(results) < truckCapacity:
+            results.append(d[1])
+
+    return results
+
+
+if __name__ == '__main__':
+    result = closestLocations(3, [[1, 2], [3, 4], [1, -1]], 2)
+    print(result)
